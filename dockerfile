965d041d4e78492b9cbc1b76c3a0688593306ff6
forestus <guilhermepm164@gmail.com>
2021-08-15 18:29:37 -0300

    test deploy heroku

--- dockerfile
+++ dockerfile
@@ -1,15 +1,12 @@
-FROM  node
+FROM node:14.15.4-alpine3.12
 
-WORKDIR /usr/app
+RUN apk add --no-cache bash
 
-COPY package.json ./
+ENV DOCKERIZE_VERSION v0.6.1
+RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
+    && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
 
-RUN  npm install
+USER node
 
-COPY . .
-
-EXPOSE 3333
-
-
-CMD ["npm", "run", "dev"]
-
+WORKDIR /home/node/app
--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -11,8 +11,20 @@ export default async (): Promise<Connection> => {
         port: process.env.DATABASE_PORT,
         password: process.env.DATABASE_PASSWORD,
         database: process.env.DATABASE_NAME,
-        entities: [`./src/modules/**/entities/*.js`],
+        entities: [`./src/modules/**/infra/typeorm/entities/*.js`],
         migrations: [`./src/shared/infra/typeorm/migrations/*.js`],
+      })
+    );
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return createConnection(
+      Object.assign(defaultOptions, {
+        host: process.env.DATABASE_HOST,
+        username: process.env.DATABASE_USERNAME,
+        port: process.env.DATABASE_PORT,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
       })
     );
   }
