5112ac628270486370598322ec146a8fe0171be2
forestus <guilhermepm164@gmail.com>
2021-08-15 05:25:49 -0300

    teste deploy heroku

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -3,6 +3,11 @@ import { Connection, createConnection, getConnectionOptions } from "typeorm";
 export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
   if (process.env.NODE_ENV === "production") {
+    defaultOptions.host = process.env.DATABASE_HOST
+    defaultOptions.username = process.env.DATABASE_USERNAME
+    defaultOptions.port = process.env.DATABASE_PORT
+    defaultOptions.password = process.env.DATABASE_PASSWORD
+    defaultOptions.database = process.env.DATABASE_NAME
     defaultOptions.entities[0] = "./src/modules/**/entities/*.js";
     defaultOptions.migrations[0] = "./src/shared/infra/typeorm/migrations/*.js";
   }
